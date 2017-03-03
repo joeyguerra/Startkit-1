3a63f519b4895a1107d0dc579ffcadf4f680a79d
Wesley Xie <WesleyXie0424@gmail.com>
2017-03-04 00:51:03 +0800

    Introduce AsyncTask and Fragment

--- app/src/main/java/com/thoughtworks/android/startkit/MainActivity.java
+++ app/src/main/java/com/thoughtworks/android/startkit/MainActivity.java
@@ -1,70 +1,19 @@
 package com.thoughtworks.android.startkit;
 
-import android.content.Context;
-import android.graphics.BitmapFactory;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ImageView;
-import android.widget.ListView;
-import android.widget.RatingBar;
-import android.widget.TextView;
-
-import com.thoughtworks.android.startkit.book.Book;
-import com.thoughtworks.android.startkit.book.Data;
-
-import java.util.List;
-
-import static com.thoughtworks.android.startkit.book.DataLoader.loadJSONData;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "Main";
-
-    private ListView mListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Data data = Data.from(loadJSONData(this));
-        mListView = (ListView) findViewById(android.R.id.list);
-        mListView.setAdapter(new MyArrayAdapter(this, data.getBookArray()));
-    }
-
-    static class MyArrayAdapter extends ArrayAdapter<Book> {
-
-        public MyArrayAdapter(Context context, List<Book> books) {
-            super(context, 0, books);
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-
-            LayoutInflater inflater = LayoutInflater.from(getContext());
-            View view = inflater.inflate(R.layout.list_item_book, parent, false);
-
-            TextView title = (TextView) view.findViewById(R.id.title);
-            TextView information = (TextView) view.findViewById(R.id.information);
-            TextView summary = (TextView) view.findViewById(R.id.summary);
-            ImageView image = (ImageView) view.findViewById(R.id.thumbnail);
-            RatingBar ratingBar = (RatingBar) view.findViewById(R.id.rating);
-            TextView ratingVal = (TextView) view.findViewById(R.id.ratingValue);
-
-            Book data = getItem(position);
-
-            title.setText(data.getTitle());
-            summary.setText(data.getSummary());
-            information.setText(data.getInformation());
-            ratingBar.setRating((float) (data.getRating() / 2));
-            ratingVal.setText(String.valueOf(data.getRating()));
-            image.setImageBitmap(BitmapFactory.decodeResource(getContext().getResources(), R.drawable.ic_default_cover));
-
-            return view;
+        if (savedInstanceState == null) {
+            getFragmentManager().beginTransaction().add(R.id.container, new BooklistFragment()).commit();
         }
     }
 }
